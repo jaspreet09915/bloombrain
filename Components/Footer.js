8e41f3ee7b79a10fa33497e0bb3fa2f4f21f80bd
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-05-30 02:51:40 +0530

    Footer template is ready

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,9 +1,184 @@
 import React from 'react'
-
+import Image from 'next/image'
+import { SiFacebook } from 'react-icons/si';
+import {TiSocialTwitterCircular} from 'react-icons/ti';
+import {FiInstagram} from 'react-icons/fi';
+import {AiOutlineYoutube} from 'react-icons/ai';
+import {GiWhiteBook} from 'react-icons/gi';
+// 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div><section class="text-white text-sm text-red body-font bg-stone-900 mb-0 pb-5">
+  <div class="container px-5 py-10 mx-auto">
+    
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-blue-500 mb-4 text-sm text-center sm:text-left">JUNIOR CLASSES</h2>
+        <nav class="flex flex-col text-white sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+              <GiWhiteBook/>
+            </span>CBSE Class 1
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>CBSE Class 2
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>CBSE Class 3
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>CBSE Class 4
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>CBSE Class 5
+          </a>
+        </nav>
+      </div>
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-blue-500 mb-4 text-sm text-center sm:text-left">SENIOR CLASSES</h2>
+        <nav class="flex flex-col text-white sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>First Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Second Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Third Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Fourth Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Fifth Link
+          </a>
+        </nav>
+      </div>
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-blue-500 mb-4 text-sm text-center sm:text-left">LIVE CLASSES</h2>
+        <nav class="flex flex-col text-white sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>First Link
+          </a>
+          <a>
+            <span class="text-white   mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Second Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Third Link
+          </a>
+          <a>
+            <span class=" text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Fourth Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Fifth Link
+          </a>
+        </nav>
+      </div>
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-blue-500 mb-4 text-sm text-center sm:text-left">QUICK LINKS</h2>
+        <nav class="flex text-white flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+          <a>
+            <span class="text-white  mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>First Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Second Link
+          </a>
+          <a>
+            <span class="  text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Third Link
+          </a>
+          <a>
+            <span class="text-white  mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook />
+            </span>Fourth Link
+          </a>
+          <a>
+            <span class="text-white mr-1 inline-flex items-center justify-center">
+            <GiWhiteBook/>
+            </span>Fifth Link
+          </a>
+        </nav>
+      </div>
+    </div>
+    <button class="flex mx-auto mt-10 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+  </div>
+  <hr className='border-solid border-1 border-white  shadow-xl w-11/12 mx-auto' />
+  <div class="text-white text-sm text-center mb-10 mt-5">
+      <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-zinc-400 mb-4">Amazing stuff about our Company</h1>
+      <p class="text-white text-sm  leading-relaxed xl:w-11/12 lg:w-full mx-auto ">
+      Bloombrain LIVE Tuition Classes is a wonderful personalized teaching platform which includes its 100% to engage children with original interactive activities, books, animated videos, games, and creative lessons captivate children’s attention. With a personalized curriculum and highly qualified teachers, it aims to provide live tuition classes from home with the best learning experience. To make education accessible for all students through online live classes, we provide teachers who are experts in CBSE Board and have immense knowledge in eclectic fields of study. Under the guidance of these educators, the curriculum we provide in our online tuition classes is comprehensive and has been designed in consultation with teachers and learning experts where it covers proficiency in various topics of different subjects. Topics cover understanding by Design Habits of Mind, Social Emotional Learning, Cultural and Linguistic Diversity, Personalized Learning, and Growth Mindset, etc.
+      </p>
+      <p class="text-white text-sm  leading-relaxed xl:w-11/12 lg:w-full mx-auto my-1 ">
+      Being a global educational platform and the best online CBSE coaching, Bloombrain inspires a lifetime of learning and discovery with its free, fun educational program for all students of classes 1st-9th. Through our online tuition classes, your children can explore the immersive world of Bloombrain in their own way – by following the progressive Step-by-Step Learning Path or by delving deep into the topics or activities that inspire them. To make it easier for students to grasp knowledge in the online live classes, teachers create interactive lessons and collaborative spaces for their virtual classroom with real-time tools. These are interactive visual communication tools which are used to create presentations, interactive images, infographics, and more.
+      </p>
+      <p class="text-white text-sm  leading-relaxed xl:w-11/12 lg:w-full mx-auto ">
+      Not only this, Bloombrain also provides everything needed to learn computer programming in a fun way. Being one of the best CBSE online tuitions, it empowers the creativity of students of Classes 1-9 of CBSE Board and serves numerous educators worldwide. If you are facing difficulties in finding the best home tutor online for your child, reach out to us. Bloombrain’s online teachers are experienced computer software developers, designers, and educators who double as mentors to make coding fun and engaging for kids. Through their impeccable teaching methodology in our online tuition classes, they encourage the students to create professional grade games and software of their own.
+      </p>
+      <p class="text-white text-sm  leading-relaxed xl:w-11/12 lg:w-full mx-auto ">
+      Furthermore, Bloombrain helps students learn the important aspects to character development, such as leadership and entrepreneurial skills in its Personality Development Courses. In its online classes for kids, it incorporates the teaching methodology which contains thousands of methods for improving body language, voice, emotions, and speech crafting. Brainstorming, ideation, and discussions happen in real-time. A pre and post session test is taken to understand the concept clarity of the kid. While following such a teaching approach, the teachers make sure to stay in touch and track student progress.
+      </p>
+      <p class="text-white text-sm  leading-relaxed xl:w-11/12 lg:w-full mx-auto ">
+      Along with this, teachers also communicate about the same to kids and their parents, too. This enables the parents to move along with their children in their learning journey. Hence, this proves Bloombrain to be different and efficient than other home tutor websites.
+      </p>
+    </div>
+    <hr className='border-solid border-1 border-white  shadow-xl w-11/12 mx-auto ' />
+    <div className='w-11/12 mx-7'>
+        <div className=' flex my-4 justify-between'>
+          <div className=' ml-10 my-4  '>
+          <Image
+            src="/Bloombrain-logo.png"
+            alt="Picture of the author"
+            width={130}
+            height={100}
+          />
+          </div>
+          <div className=' my-4 py-8 '>
+          <p>© 2020 All Right Reserved.</p>
+          </div>
+          <div className='  my-4  flex py-8  '>
+          <SiFacebook 
+          className='text-blue-400 text-3xl mx-1 ' />
+          <TiSocialTwitterCircular className='mx-1  text-3xl text-blue-400' />
+          <FiInstagram className='text-3xl mx-1  text-blue-400 '/>
+          <AiOutlineYoutube className='text-3xl mx-1  text-blue-400 '/>
+          </div>
+        </div>
+    </div>
+    
+</section></div>
   )
 }
-
 export default Footer
